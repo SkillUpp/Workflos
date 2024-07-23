e384f4cb1c317653d4ca879dec7bc145d75e1f50
zhang-shiqiang <50956988+zhang-shiqiang@users.noreply.github.com>
2024-07-23 17:04:19 +0800

    # 代码测试

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,15 +1,22 @@
 import ProductDetail from "@/app/detail/index";
 
-export async function generateStaticParams() {
-  const paths = [
-    { id: "ClickUp" },
-    { id: "Slack" },
-    { id: "Trello" },
-    { id: "Clio" },
-  ];
+interface IPathItem {
+  params: {
+    id: string;
+  };
+}
+
+export function generateStaticParams(id: any) {
+  const paths: IPathItem[] = [];
+  paths.push({ params: { id } });
+
+  console.log(paths, 'paths');
+  
   return paths;
 }
 
 export default async function PostPage({ params }: { params: { id: string } }) {
+  generateStaticParams(params.id);
+
   return <ProductDetail id={params.id} />;
 }
